542dd3bc24ebc95b80545f40c11ac2e2b4d8523f
Christopher Brox <brox@google.com>
2025-11-06 10:26:50 -0800

    adding chart to code execution

--- gemini/03_tools/code_execution.py
+++ gemini/03_tools/code_execution.py
@@ -1,5 +1,6 @@
 from google.genai import Client, types
 import google.auth
+import json
 
 _, project_id = google.auth.default()
 
@@ -8,8 +9,9 @@ client = Client(vertexai=True, project=project_id, location="global")
 
 response = client.models.generate_content(
     model="gemini-flash-latest",
-    contents="Calculate 20th fibonacci number. Then find the nearest palindrome to it.",
+    contents="3 people like red, 2 people blue, 1 person likes green. Make this a pie chart. Return the chart as an image",
     config=types.GenerateContentConfig(
+        system_instruction="You are a data visualzation agent. Your output should be an image.",
         tools=[
             types.Tool(code_execution=types.ToolCodeExecution())
         ],
@@ -20,3 +22,9 @@ print("# Code:")
 print(response.executable_code)
 print("# Outcome:")
 print(response.code_execution_result)
+
+
+for part in response.candidates[0].content.parts:
+    if part.inline_data:
+        print(part.inline_data.as_image().save("pie.png"))
+

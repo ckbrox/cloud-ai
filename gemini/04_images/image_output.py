afca72620ab995aee2097674e6f87034cc2809b5
Christopher Brox <brox@google.com>
2025-11-24 08:54:58 -0800

    adding image spelling, gemini pro image

--- gemini/04_images/image_output.py
+++ gemini/04_images/image_output.py
@@ -6,7 +6,7 @@ _, project_id = google.auth.default()
 client = Client(vertexai=True, project=project_id, location="global")
 
 resposne = client.models.generate_content(
-    model='gemini-2.5-flash-image',
+    model='gemini-3-pro-image-preview',
     contents=["make a cute puppy"],
     config=types.GenerateContentConfig(
         response_modalities=['IMAGE', 'TEXT']

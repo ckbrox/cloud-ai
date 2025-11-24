a928fe6c8a2bd61900b581d8e8474eca90f57613
Christopher Brox <brox@google.com>
2025-11-24 08:56:43 -0800

    updating spell word from person

--- gemini/04_images/image_spelling.py
+++ gemini/04_images/image_spelling.py
@@ -6,33 +6,33 @@ _, project_id = google.auth.default()
 
 client = Client(vertexai=True, project=project_id, location="global")
 
-message = 'GET WELL'
 
-with open('source.png', 'rb') as f:
-    image_part = types.Part.from_bytes(data=f.read(), mime_type='image/png')
+def spell_word_from_person(word: str, source_image: str, source_image_mime_type: str='image/png'):
+
+    with open(source_image, 'rb') as f:
+        image_part = types.Part.from_bytes(data=f.read(), mime_type=source_image_mime_type)
 
 
-
-for letter in message:
-    if letter == ' ':
-        continue
-    
-    resposne = client.models.generate_content(
-        model='gemini-3-pro-image-preview',
-        contents=[image_part, f'Make the man contort his body in the shape of the letter {letter} on a flat white background. His shape should be clearly the letter {letter} with no extras'],
-        config=types.GenerateContentConfig(
-            response_modalities=['IMAGE']
-        )
-    )
-
-    candidate = resposne.candidates[0] 
-    for index, part in enumerate(candidate.content.parts):
-        if part.text:
-            print(part.text)
+    for letter in word:
+        if letter == ' ':
+            continue
         
-        if inline_data := part.inline_data:
-            image_name = f'{letter}-{uuid4().hex}.png'
-            with open(image_name, 'wb') as f:
-                f.write(inline_data.data)
+        resposne = client.models.generate_content(
+            model='gemini-3-pro-image-preview',
+            contents=[image_part, f'Make the man contort his body in the shape of the letter {letter} on a flat white background. His shape should be clearly the letter {letter} with no extras'],
+            config=types.GenerateContentConfig(
+                response_modalities=['IMAGE']
+            )
+        )
+
+        candidate = resposne.candidates[0] 
+        for index, part in enumerate(candidate.content.parts):
+            if part.text:
+                print(part.text)
+            
+            if inline_data := part.inline_data:
+                image_name = f'{letter}-{uuid4().hex}.png'
+                with open(image_name, 'wb') as f:
+                    f.write(inline_data.data)
 
 

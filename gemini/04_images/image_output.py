c7c00f12ed7dfbc1ecff8d40fa8fd465f9c56577
Christopher Brox <brox@google.com>
2026-02-10 10:38:25 -0800

    adding generation id to image output

--- gemini/04_images/image_output.py
+++ gemini/04_images/image_output.py
@@ -1,24 +1,32 @@
 from google.genai import Client, types
 import google.auth
+from uuid import uuid4
 
 _, project_id = google.auth.default()
 
 client = Client(vertexai=True, project=project_id, location="global")
 
-resposne = client.models.generate_content(
+response = client.models.generate_content(
     model='gemini-3-pro-image-preview',
-    contents=["make a cute puppy"],
+    contents=["make 3 images of a cute puppy jumping over a wall"],
     config=types.GenerateContentConfig(
         response_modalities=['IMAGE', 'TEXT']
     )
 )
 
-candidate = resposne.candidates[0]
+
+
+
+candidate = response.candidates[0]
+generation_id = uuid4().hex
 for index, part in enumerate(candidate.content.parts):
     if part.text:
         print(part.text)
     
     if inline_data := part.inline_data:
-        with open(f'response_image_{index}.png', 'wb') as f:
+        with open(f'temp/response_image_{generation_id}-{index}.png', 'wb') as f:
             f.write(inline_data.data)
         print(f'response_image_{index}.png')
+
+
+print(response.usage_metadata.model_dump_json(indent=4))
